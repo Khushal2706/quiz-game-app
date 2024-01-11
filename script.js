2514980b970d4c7e1c00bd49cf2ae41c1e187db7
Khushal2706 <khushal2706patil@gmail.com>
2024-01-11 15:45:45 +0530

    added some javaScript

---
+++ script.js
@@ -0,0 +1,7 @@
+const questionobj ={
+    category: "food and drink",
+    id: "qa-1",
+    correctAnswer: "three",
+    options: ["two","three","four","five"],
+    question: "How many pieces of buns are in a Mcdonalds big Mac?"
+}
