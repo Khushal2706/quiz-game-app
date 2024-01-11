c45f7ab0a42089abb24056ac803c8b878c254559
Khushal2706 <khushal2706patil@gmail.com>
2024-01-11 16:09:02 +0530

    added option for  the questions

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ const questionobj ={
     options: ["two","three","four","five"],
     question: "How many pieces of buns are in a Mcdonald's big Mac?"
 };
+// destructuring the needy elements
 const {correctAnswer, options, question} = questionobj;
 const questionEl = document.getElementById("question");
+const optionEl = document.getElementById("options");
+const scoreEl = document.getElementById("score");
+// manipulating the dom 
 questionEl.textContent = question;
+//  adding buttons on the option div
+options.forEach((opt) =>{
+    const btn = document.createElement("button");
+    btn.textContent = opt;
+    optionEl.appendChild(btn);
+})
